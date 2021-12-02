05b933a7e8e71b407b6964bc060f883ca81021a7
jxes6102 <jxes6102@gmail.com>
2021-12-02 18:33:38 +0800

    to page

---
+++ precache-manifest.f223812b2cf4b4818d5ea48d67708ece.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84395aebafde8b187d49",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "78f70631d0c7ea225793",
+    "url": "css/chunk-vendors.css"
+  },
+  {
+    "revision": "9d3917cdbd60c4e15676d75eb9bdb15d",
+    "url": "index.html"
+  },
+  {
+    "revision": "c69584d5a2de5dd80a97",
+    "url": "js/about.js"
+  },
+  {
+    "revision": "84395aebafde8b187d49",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "78f70631d0c7ea225793",
+    "url": "js/chunk-vendors.js"
+  },
+  {
+    "revision": "cb32f5e99d12ecd67f8bf3e62267d3f3",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
